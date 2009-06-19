599e6c9ae57cc14221e7f465132b4eb8805c0b9e
eoigal <eoin@automattic.com>
2009-06-19 11:04:08 +0000

    Updated to version 1.2 Fixes for SSL login failures and login refresh
    git-svn-id: https://plugins.svn.wordpress.org/polldaddy/tags/1.2@127328 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ polldaddy.js
@@ -0,0 +1,78 @@
+jQuery(function($){
+	$('.hide-if-js').hide();
+	$('.hide-if-no-js').removeClass( 'hide-if-no-js' );
+
+	$('a.delete-poll').click( function() {
+		return confirm( 'Are you sure you want to delete "' + $(this).parents( 'td' ).find( 'strong' ).text() + '"?' );
+	} );
+
+	$('span.view a.thickbox').attr( 'href', function() {
+		return $(this).attr( 'href' ) + '&iframe&TB_iframe=true';
+	} );
+
+	var delAnswerPrep = function( context ) {
+		$('a.delete-answer', context || null ).click( function() {
+			if ( confirm( 'Are you sure you want to delete this answer?' ) ) {
+				$(this).parents( 'li' ).remove();
+			}
+			return false;
+		} );
+	};
+	delAnswerPrep();
+
+	$('#answers').sortable( {
+		axis: 'y',
+		containment: 'parent',
+		handle: '.handle',
+		tolerance: 'pointer'
+	} );
+
+	$('#add-answer-holder').show().find( 'button').click( function() {
+		var aa = ( 1 + $('#answers li').size() ).toString();
+		delAnswerPrep( $('#answers').append( '<li><span class="handle">&#x2195;</span><div><input type="text" name="answer[new' + aa + ']" size="30" tabindex="2" value="" autocomplete="off" /></div><a title="delete this answer" class="delete-answer delete" href="#">&times;</a></li>' ).find( 'li:last' ) );
+		return false;
+	} );
+
+	var win = window.dialogArguments || opener || parent || top;
+	$('.polldaddy-send-to-editor').click( function() {
+		var pollID = $(this).siblings('.polldaddy-poll-id').val();
+		if ( !pollID )
+			pollID = $('.polldaddy-poll-id:first').val();
+		win.send_to_editor( '[polldaddy poll=' + parseInt( pollID ).toString() + ']' );
+	} );
+
+	$('.polldaddy-show-shortcode').toggle( function() {
+		$(this).parents('tr:first').next('tr').fadeIn();
+		return false;
+	}, function() {
+		$(this).parents('tr:first').next('tr').fadeOut();
+		return false;
+	} );
+
+	var img1 = $('#design img:first');
+	if ( !img1.size() ) {
+		return;
+	}
+	var img2 = $('#design img:last');
+	var imgPath = 'http://polldaddy.com/images/';
+
+	var styleCount = $(':input[name=styleID] option').size();
+	var styles = $(':input[name=styleID]').remove();
+	var o = parseInt( styles.val() );
+	$('#design').append( '<input type="hidden" id="hidden-styleID" name="styleID" value="' + o.toString() + '" /><p><strong id="styleID-name">' + $(styles.find('option').get(o)).text() + '</strong><br /><span id="span-styleID">' + ( o + 1 ).toString() + '</span> of ' + styleCount + '</p>');
+	var hiddenStyleID = $('#hidden-styleID');
+	var spanStyleID = $('#span-styleID');
+	var styleIDName = $('#styleID-name');
+
+	var changePreview = function( i ) {
+		var o = parseInt( img1.attr( 'src' ).substr( imgPath.length ) );
+		img1.attr( 'src', imgPath + ( ( i + o + styleCount ) % styleCount ).toString() + '.gif' );
+		img2.attr( 'src', imgPath + ( ( 2 * i + o + styleCount ) % styleCount ).toString() + '.gif' );
+		hiddenStyleID.val( ( ( i + o + styleCount ) % styleCount ).toString() );
+		spanStyleID.text( ( ( i + o + styleCount ) % styleCount + 1 ).toString() );
+		styleIDName.text( $(styles.find('option').get( ( i + o + styleCount ) % styleCount )).text() );
+	};
+
+	$('#design a.alignleft').click( function() { changePreview( -1 ); return false; } );
+	$('#design a.alignright').click( function() { changePreview( 1 ); return false; } );
+});
---
+++ readme.txt
@@ -0,0 +1,84 @@
+=== PollDaddy ===
+Contributors: mdawaffe, eoigal
+Tags: poll, polls, polldaddy, WordPress.com
+Requires at least: 2.6
+Tested up to: 2.7.1
+Stable tag: 1.2
+
+Create and manage PollDaddy polls from within WordPress.
+
+== Description ==
+
+The PollDaddy plugin allows you to create and manage your [PollDaddy.com](http://polldaddy.com/) polls from within your WordPress blog's administration area.
+
+== Installation ==
+
+Upload the plugin to your blog, Activate it, then enter your PollDaddy.com email address and password.
+
+== Frequently Asked Questions ==
+
+= I have multiple authors on my blog?  What happens? =
+
+Each author that wants to create polls will need his or her own PollDaddy.com account.
+
+= But, as an Administrator, can I edit my Authors' polls =
+
+Yes. You'll be able to edit the polls they create from your blog.  (You won't be able to edit any of their non-blog, personal polls they create through PollDaddy.com.)
+
+= Neat! Um... can my Authors edit MY blog polls? =
+
+Nope.  The permissions are the same as for posts.  So Editors and Administrators can edit anyone's polls for that blog.  Authors can only edit their own.
+
+== Change Log ==
+
+= 1.2 =
+Bug Fix: SSL request for PollDaddy API key sometimes failed due to host constraints, included option to make a normal http request in this case.
+Bug Fix: Redirect after login now goes to list polls
+
+= 1.1 =
+Bug Fix: Don't call PollDaddy API on every admin page load
+Bug Fix: Correct Image locations
+Bug Fix: CSS Tweaks for upcoming WordPress 2.8
+Make Javascript image selector more robust
+
+= 1.0 =
+* New PollDaddy API
+* Do not store UserCode, retrieve from API
+* Bug Fix: Fix API key retrieval.  Improper use of wp_remote_post()
+
+= 0.9 =
+* Compatiblity with WordPress 2.7
+* Bug Fix: Potential charset issues
+
+= 0.8 =
+* Bug fix: prevent some PHP define errors
+* Bug fix: send content-length header when using wp_remote_post()
+
+= 0.7 =
+* Potential bug fix: Maybe get rid of 'Invalid Poll Author' error... again.
+
+= 0.6 =
+* Bug fix: Allow reauthentication with PollDaddy email address and PollDaddy password.  This is necessary because the stored PollDaddy User API key is invalidated if the user's details change on PollDaddy.com.
+* Buf fix: Speed up CSS and JS.
+* Feature: Link to view Shortcode and JavaScript code for each poll.
+
+= 0.5 =
+* A few more helpful error messages.
+* Bug fix: Password field should be a password field, not a text field.
+* Potential bug fix: Maybe get rid of 'Invalid Poll Author' error.
+
+= 0.4 =
+* Bug fix: Shortcode handler was commented out in earlier versions.
+* Bug fix: PHP Warning: in_array() [function.in-array]: Wrong datatype for second argument in polldaddy-xml.php on line 78
+
+= 0.3 =
+* Bug fix: Send text data escaped in CDATA to prevent XML errors.
+* Bug fix: Append to text value, don't overwrite it.  XML parser can call text handler many times per node.
+* Bug fix: No more slashes when a poll reloads in the edit form after an error.
+
+= 0.2 =
+* Bug fix: Get rid of slashes.
+* Bug fix: PHP Fatal Error: call to undefined function
+
+= 0.1 =
+* Initial release
