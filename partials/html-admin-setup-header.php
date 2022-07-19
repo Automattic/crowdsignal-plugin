3ef96f0ee2ec199adea477adcc728c579344b45c
Christian Gastrell <cgastrell@gmail.com>
2022-07-19 18:02:13 -0300

    whitespace fix

--- partials/html-admin-setup-header.php
+++ partials/html-admin-setup-header.php
@@ -13,41 +13,41 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div id="cs-plugin-container">
 		<div class='cs-lower'>
 			<h1 id='crowdsignal__logo'><?php esc_html_e( 'Crowdsignal Settings', 'crowdsignal-forms' ); ?></h1>
-	<?php
-	// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-	if ( isset( $_GET['msg'] ) ) {
-		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		switch ( $_GET['msg'] ) {
-			case 'disconnect-fail':
-				echo '<div class="error fade crowdsignal-message"><p>' .
-				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
-				esc_html__( 'Could not disconnect. Please try again.', 'crowdsignal-forms' ) .
-				'</p></div>';
-				break;
-			case 'disconnected':
-				echo '<div class="updated fade crowdsignal-message"><p>' .
-				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
-				esc_html__( 'Successfully disconnected from Crowdsignal.', 'crowdsignal-forms' ) .
-				'</p></div>';
-				break;
-			case 'connected':
-				echo '<div class="updated crowdsignal-message"><p>' .
-				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
-				esc_html__( 'Success! Your Crowdsignal account is successfully connected! You are ready!', 'crowdsignal-forms' ) .
-				'</p></div>';
-				break;
-			case 'api-key-added':
-				echo '<div class="updated crowdsignal-message"><p>' .
-				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
-				esc_html__( 'You have been connected to Crowdsignal.', 'crowdsignal-forms' ) .
-				'</p></div>';
-				break;
-			case 'api-key-not-added':
-				echo '<div class="error fade crowdsignal-message"><p>' .
-				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
-				esc_html__( 'Your API key has not been updated, please try again.', 'crowdsignal-forms' ) .
-				'</p></div>';
-				break;
-		}
-	}
-	?>
+			<?php
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			if ( isset( $_GET['msg'] ) ) {
+				// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+				switch ( $_GET['msg'] ) {
+					case 'disconnect-fail':
+						echo '<div class="error fade crowdsignal-message"><p>' .
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
+						esc_html__( 'Could not disconnect. Please try again.', 'crowdsignal-forms' ) .
+						'</p></div>';
+						break;
+					case 'disconnected':
+						echo '<div class="updated fade crowdsignal-message"><p>' .
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
+						esc_html__( 'Successfully disconnected from Crowdsignal.', 'crowdsignal-forms' ) .
+						'</p></div>';
+						break;
+					case 'connected':
+						echo '<div class="updated crowdsignal-message"><p>' .
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
+						esc_html__( 'Success! Your Crowdsignal account is successfully connected! You are ready!', 'crowdsignal-forms' ) .
+						'</p></div>';
+						break;
+					case 'api-key-added':
+						echo '<div class="updated crowdsignal-message"><p>' .
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
+						esc_html__( 'You have been connected to Crowdsignal.', 'crowdsignal-forms' ) .
+						'</p></div>';
+						break;
+					case 'api-key-not-added':
+						echo '<div class="error fade crowdsignal-message"><p>' .
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is svg from internal lib
+						esc_html__( 'Your API key has not been updated, please try again.', 'crowdsignal-forms' ) .
+						'</p></div>';
+						break;
+				}
+			}
+			?>
